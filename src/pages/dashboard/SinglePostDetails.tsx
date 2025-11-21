a8c12644bf1918259c93c918f0976ad40e1f9dc9
Folacodes <fola4ril@gmail.com>
2025-11-21 11:49:17 +0100

    working on nested replies

--- src/pages/dashboard/SinglePostDetails.tsx
+++ src/pages/dashboard/SinglePostDetails.tsx
@@ -1,3 +1,4 @@
+import Comment from "@/components/atoms/Comment";
 import ViewPost from "@/components/cards/ViewPost";
 import CommentBox from "@/components/CommentBox";
 import { Loader } from "@/components/molecules/Loader";
@@ -28,6 +29,7 @@ const SinglePostDetails = () => {
           <Loader />
         ) : (
           <div className="">
+            {/* Original Post */}
             <ViewPost
               publicId={data?.publicId}
               profileName={profileData?.data?.displayName}
@@ -40,30 +42,38 @@ const SinglePostDetails = () => {
               reactionsData={transformReactions(data?.reactions)}
             />
 
+            {/* Comment Box for Top-Level Comments */}
             <div className="mt-2 bg-grey_20 p-4">
               <CommentBox
                 ifPoll
                 ifRecord
                 endpoint={`contents/${params?.id}/comments`}
-                // setIfUserIsCreatingPoll={setIfUserIsCreatingPoll}
+                placeholder="Write a comment..."
               />
             </div>
 
-            {data?.data?.comments.map((data: StoryPost) => {
-              return (
-                <ViewPost
-                  publicId={data?.publicId}
-                  profileName={profileData?.data?.displayName}
-                  avatar={profileData?.data?.profilePic}
-                  handle={`@${profileData?.data?.username}`}
-                  time={formatTimeAgo(data?.createdDate)}
-                  paragraphOne={data?.message}
-                  timeLineImage={data?.mediaFiles}
-                  ifParagraph={true}
-                  reactionsData={transformReactions(data?.reactions)}
-                />
-              );
-            })}
+            {/* Comments Section */}
+            <div className="mt-4">
+              {data?.data?.comments?.length > 0 ? (
+                <>
+                  <h3 className="text-lg font-semibold px-4 mb-4">
+                    Comments ({data.data.comments.length})
+                  </h3>
+                  {data.data.comments.map((comment: StoryPost) => (
+                    <Comment
+                      key={comment.publicId}
+                      comment={comment}
+                      profileData={profileData}
+                      postId={params?.id || ""}
+                    />
+                  ))}
+                </>
+              ) : (
+                <div className="text-center py-8 text-grey_60">
+                  No comments yet. Be the first to comment!
+                </div>
+              )}
+            </div>
           </div>
         )}
       </>
